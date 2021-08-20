3900246d3b47fb0a76c58d9e31af3fd0b43e1e59
LiadTZ <liadt@post.bgu.ac.il>
2021-08-20 17:55:28 +0300

    home work 3 - liad

---
+++ CV.js
@@ -0,0 +1,5 @@
+
+function myFunction(){
+    document.getElementById("h1").innerHTML = "Liad An undergraduate student for Industrial Engineering and Management";
+    document.getElementById("title").innerHTML = "Liad An undergraduate student for Industrial Engineering and Management";
+}
