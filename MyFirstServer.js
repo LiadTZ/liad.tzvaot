527ec2a3035bb6c0ed21440bcf344922f68da6db
LiadTZ <88494590+LiadTZ@users.noreply.github.com>
2021-09-21 19:49:01 +0300

    home work 6

--- MyFirstServer.js
+++ MyFirstServer.js
@@ -1,3 +1,8 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+
 var http = require('http');
 
 
@@ -7,3 +12,24 @@ res.end('Hello World');
 })
 
 .listen(8080);
+
+const mysql = require("mysql");
+const dbConfig = require(“./db.config.js");
+// Create a connection to the database
+const connection = mysql.createConnection({
+host: dbConfig.HOST,
+user: dbConfig.USER,
+password: dbConfig.PASSWORD,
+database: dbConfig.DB
+});
+// open the MySQL connection
+connection.connect(error => {
+if (error) throw error;
+console.log("Successfully connected to the database.");
+});
+module.exports = connection;
+
+ // new route
+ app.get('path', function(req,res){
+ res.send('');
+ });
