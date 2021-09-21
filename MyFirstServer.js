a46a1529f51eb83e5ae0a6ca229fbc14a04f6b31
LiadTZ <88494590+LiadTZ@users.noreply.github.com>
2021-09-21 20:05:30 +0300

    Home Work 6 - server

--- MyFirstServer.js
+++ MyFirstServer.js
@@ -1,11 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-
+const mysql = require("mysql");
+const dbConfig = require("./db.config.js");
 
 var http = require('http');
-
-
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
 res.end('Hello World');
@@ -13,8 +9,6 @@ res.end('Hello World');
 
 .listen(8080);
 
-const mysql = require("mysql");
-const dbConfig = require(“./db.config.js");
 // Create a connection to the database
 const connection = mysql.createConnection({
 host: dbConfig.HOST,
@@ -28,6 +22,20 @@ if (error) throw error;
 console.log("Successfully connected to the database.");
 });
 module.exports = connection;
+
+// Create a route 
+app.get("/id", function(req, res){
+    sql.query("SELECT * FROM id", (err, mysqlres) => {
+    if (err) {
+    console.log("error: ", err);
+    res.status(400).send({message: "error : " + err});
+    return;
+    }
+    console.log("all");
+    res.send(mysqlres);
+    return;
+    });
+    });
 
  // new route
  app.get('path', function(req,res){
